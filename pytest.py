7e01e6324078afadc6a50e423a4b75bb3be635b7
Noah Vendrig <noahvendrig@gmail.com>
2022-06-09 10:55:35 +1000

    started tkinter ui

--- pytest.py
+++
@@ -1,9 +0,0 @@
-class Classifier:
-    def __init__(self, labels, X, y):
-        self.labels = labels
-        self.X = X
-        self.y = y
-
-
-c1 = Classifier(['normal', 'covid', 'pneumonia'], [], [])
-print(c1.labels)
