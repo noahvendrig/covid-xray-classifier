db49440248522ad18a7e5d949ba22729d951425d
Noah Vendrig <noahvendrig@gmail.com>
2022-06-20 12:06:44 +1000

    IT WORKS HAHAHA

--- predict.py
+++ predict.py
@@ -16,20 +16,24 @@ class Classifier:
 
 
     def ImageToArray(self, file):
-        img_arr = cv2.imread(file)   # reads an image in the BGR format
+           # reads an image in the BGR format
         try:
+            img_arr = cv2.imread(file)
             img_arr = cv2.cvtColor(img_arr, cv2.COLOR_BGR2RGB)   # BGR -> RGB
             return img_arr
         except Exception as e:
             print("INCORRECT FILE PATH")
-            return
+            import os
+            os._exit(777) # exit the program but no error message to clog terminal when debuggig
+            # return
         
     
     
     def Classify(self, img):
-        actual_label = img.split("/")[-1]
-        new_img = self.ImageToArray(img)
-        new_img = (new_img[::2, ::2]/255).astype('float32')
+        # actual_label = img.split("/")[-1]
+        new_img = cv2.resize(self.ImageToArray(img), (150,150))
+        # new_img = (new_img[::2, ::2]/255).astype('float32')
+        new_img = (new_img/255).astype('float32')
         new_img = tf.expand_dims(new_img, 0) # model expects a dataset so we expand_dims
         self.prediction = self.model.predict(new_img)
         self.prediction = np.argmax(self.prediction, axis=None, out=None) # convert from categorical back to index
@@ -39,16 +43,16 @@ class Classifier:
         # print()
         self.prediction = self.labels[self.prediction]
 
-def main():
-    print("RUNNING MAIN")
+def main(input):
+#     print("RUNNING MAIN")
     # if __name__ == '__main__':
     # Only executed if you start this script as the main script,
     # i.e. you enter 'python path/to/main.py' in a terminal.
     # Assuming you saved the script in the directory 'path/to'
     # and named it 'main.py'.
     # c = Classifier("6-conv-128-nodes-2-dense-1654694547.model", ["./dataset/normal/images/Normal-10000.png" , "./dataset/covid/images/COVID-3615.png"])
-    res = Classifier("6-conv-128-nodes-2-dense-1655171754.model", ["./dataset/normal/images/Normal-10000.png"])
+    res = Classifier("6-conv-128-nodes-2-dense-1655171754.model", input)
     return res.prediction
-# main()
+# main(["./dataset/normal/images/Normal-10000.png"])
 
-print(main())
+# print(main())
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 from predict import main
+import cv2
+
 
 UPLOAD_FOLDER = 'static/files/'
 
@@ -21,6 +23,19 @@ from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
+def resize(im):
+	print(im)
+	h, w, channels = im.shape
+
+	max_h = int(400)
+	ratio = w/h
+	resized_w, resized_h = int(round(max_h*ratio)), max_h
+	dims = (resized_w, resized_h)
+	# resized_im = im.resize(im, (resized_w, resized_h))
+	resized_im = cv2.resize(im, dims)
+	print(resized_im.shape)
+	return resized_im
+
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 	
@@ -34,28 +49,33 @@ def upload_image():
 		flash('No file part')
 		return redirect(request.url)
 	file = request.files['file']
-	if file.filename == '':
+	if file.filename == '':	
 		flash('No image selected for uploading')
 		return redirect(request.url)
 
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+		file.save(path)
 		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		prediction = main()
-		# print(prediction)
-		prediction = "eeee"
-		return render_template('i.html', filename=filename, prediction=prediction), 
+		im = cv2.imread(path) # read 
+		os.remove(path)
+		im = resize(im)
+		cv2.imwrite(path, im)
+		# flash('Image successfully uploaded and displayed below')
+		#["./dataset/normal/images/Normal-10000.png"]
+		im = [im]
+		print(path)
+		prediction = main([path])
+		return render_template('i.html', filename=filename, prediction=prediction)
 
 	else:
 		flash('Allowed image types are -> png, jpg, jpeg, gif')
 		return redirect(request.url)
 
 @app.route('/display/<filename>')
-def display_image(filename, prediction):
-	#print('display_image filename: ' + filename)
-	return redirect(url_for('static', filename='files/' + filename, prediction=prediction), code=301)
+def display_image(filename):
+	return redirect(url_for('static', filename='files/' + filename), code=301)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
